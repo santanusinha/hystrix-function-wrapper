93006fa36958aeb8737a9e2b1361b22acda8e490
Rishabh <rishabh@phonepe.com>
2019-01-12 22:11:21 +0530

    Reverted interface changes

--- src/main/java/io/appform/core/hystrix/CommandConfigurationFactory.java
+++
@@ -1,9 +0,0 @@
-package io.appform.core.hystrix;
-
-import com.netflix.hystrix.HystrixCommand;
-
-public interface CommandConfigurationFactory {
-
-    HystrixCommand.Setter provide(String group, String command);
-
-}
